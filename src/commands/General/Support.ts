9ce404de5fc60d20d8a28e93141040a8eb985d56
κακαšhï hα†ακε ☯️ <88052431+Sparky9992@users.noreply.github.com>
2022-05-29 14:53:34 +0530

    Update Support.ts

--- src/commands/General/Support.ts
+++ src/commands/General/Support.ts
@@ -19,7 +19,7 @@ export default class Command extends BaseCommand {
     run = async (M: ISimplifiedMessage): Promise<void> => {
         (await this.client.sendMessage(
         M.sender.jid,
-        `    📍My Developer's Group📍\n\n*🎇ℤ𝔼ℝ𝕆 𝕋𝕎𝕆 support group:🎇* *https://chat.whatsapp.com/KK6AVKEwPVJ0aXoWo2cK2g*\n\n`,
+        `    📍My Developer's Group📍\n\n*🎇 support group:🎇* *https://chat.whatsapp.com/HG1ryMzymgxFik23tXkOOH*\n\n`,
            MessageType.text
         ))
         const n = [
